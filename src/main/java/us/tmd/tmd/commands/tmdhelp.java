ede3908b290dd071279352a5f6c7f91f68b6a037
5vl <nederlandmax@gmail.com>
2020-12-23 15:55:41 +0100

    Added an actual usage for the help command

--- src/main/java/us/tmd/tmd/commands/tmdhelp.java
+++ src/main/java/us/tmd/tmd/commands/tmdhelp.java
@@ -1,7 +1,31 @@
 package us.tmd.tmd.commands;
 
-public class tmdhelp {
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.ChatComponentText;
 
-    
+public class tmdhelp extends CommandBase {
+
+    @Override
+    public String getCommandName() {
+        return "tmdhelp";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender iCommandSender) {
+        return null;
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+        sender.addChatMessage(new ChatComponentText("This is the TMD help command!"));
+
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 0;
+    }
 
 }
